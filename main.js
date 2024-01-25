b6255699bb31a1734181b0f99fd10b990098f0a4
Viviana Roa <viroa.rodriguez@gmail.com>
2024-01-25 00:13:26 -0300

    tamaño de imagen y Titulo de Lista de productos

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const bodega = [
-  { id: 1, nombre: "papas fritas", precio: 2590, cantidad: 4, imagen: "https://www.google.cl/url?sa=i&url=https%3A%2F%2Fwww.pngegg.com%2Fes%2Fpng-xywxg&psig=AOvVaw1arxtRuWxOOFMufmbJQHyQ&ust=1706231607283000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCKiws4iu94MDFQAAAAAdAAAAABAD" },
-  { id: 2, nombre: "arroz", precio: 980, cantidad: 3, imagen: "https://www.google.cl/imgres?imgurl=http%3A%2F%2Fwww.distribuidorasantiago.cl%2Fwp-content%2Fuploads%2F2018%2F05%2FTucapel-Arroz.jpg&tbnid=AB_u0dKRz_TyqM&vet=12ahUKEwiW_sb7wPeDAxVCP7kGHYH7B4wQMygLegQIARBs..i&imgrefurl=https%3A%2F%2Fwww.distribuidorasantiago.cl%2Fproducto%2Ftucapel-arroz-grado-2-bolsa-1-kg-manga-10-unidades-cod-777%2F&docid=pDTxmEtd0mPNkM&w=1200&h=1200&q=paquete%20de%20arroz%20png%20200px&hl=es-419&ved=2ahUKEwiW_sb7wPeDAxVCP7kGHYH7B4wQMygLegQIARBs" },
+  { id: 1, nombre: "papas fritas", precio: 2590, cantidad: 4, imagen: "https://e7.pngegg.com/pngimages/823/670/png-clipart-potato-chip-matutano-vegetarian-cuisine-cheetos-potato-food-convenience-food.png"},
+  { id: 2, nombre: "arroz", precio: 980, cantidad: 3, imagen: "https://dojiw2m9tvv09.cloudfront.net/58394/product/2-5-copy4838.png" },
   { id: 3, nombre: "fideos", precio: 540, cantidad: 6, imagen: "https://www.sm24.cl/web/image/product.template/1830/image_1024?unique=01dbf51" },
   { id: 4, nombre: "leche", precio: 1200, cantidad: 2, imagen: "https://masterofficechile.cl/wp-content/uploads/2022/04/909.jpg" },
   { id: 5, nombre: "sopas", precio: 180, cantidad: 5, imagen: "https://www.alimentosmj.cl/126-large_default/sopa-carne-con-semola-maggi-68-g.jpg" },
@@ -16,9 +16,11 @@ bodega.forEach((producto) => {
     <h3>Id: ${producto.id}</h3>
     <b>Nombre: ${producto.nombre}</b>
     <p>Precio: $${producto.precio}</p>
+    <img class="producto-imagen" src="${producto.imagen}" alt="${producto.nombre}">
   `;
+  listaProductos.append(div);
 });
-  listado.append(div);
+  
 
 localStorage.setItem("bodega", JSON.stringify(bodega));
 
