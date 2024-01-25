5b73c9a518655ed92770d995febe2fd0619b9eff
Viviana Roa <viroa.rodriguez@gmail.com>
2024-01-24 23:29:57 -0300

    agrego DOM evento y JSON

--- main.js
+++ main.js
@@ -1,32 +1,48 @@
 const bodega = [
-    {id: 1, nombre: "papas fritas", precio: 2590, cantidad: 4, imagen: },
-    {id: 2, nombre: "arroz", precio: 980, cantidad: 3, imagen:  },
-    {id: 3, nombre: "fideos", precio: 540, cantidad: 6, imagen: }
-    {id: 4, nombre: "leche", precio: 1200, cantidad: 2, imagen: },
-    {id: 5, nombre: "sopas", precio: 180, cantidad: 5, imagen:  },
-    {id: 6, nombre: "lentejas", precio: 2200, cantidad: 2, imagen: },
-    {id: 7, nombre: "conservas", precio: 1100, cantidad: 4, imagen: },
+  { id: 1, nombre: "papas fritas", precio: 2590, cantidad: 4, imagen: "" },
+  { id: 2, nombre: "arroz", precio: 980, cantidad: 3, imagen: "" },
+  { id: 3, nombre: "fideos", precio: 540, cantidad: 6, imagen: "" },
+  { id: 4, nombre: "leche", precio: 1200, cantidad: 2, imagen: "" },
+  { id: 5, nombre: "sopas", precio: 180, cantidad: 5, imagen: "" },
+  { id: 6, nombre: "lentejas", precio: 2200, cantidad: 2, imagen: "" },
+  { id: 7, nombre: "conservas", precio: 1100, cantidad: 4, imagen: "" },
 ];
 
-localStorage.getItem("bodega", JSON.stringify(bodega));
+let listado = document.getElementById("listaProductos");
+
+bodega.forEach((producto) => {
+  let div = document.createElement("div");
+  div.innerHTML = `
+    <h3>Id: ${producto.id}</h3>
+    <b>Nombre: ${producto.nombre}</b>
+    <p>Precio: $${producto.precio}</p>
+  ` 
+});
+  listado.append(div);
+
+localStorage.setItem("bodega", JSON.stringify(bodega));
+
 
 const agregoABodega = (nombre, precio, cantidad) => {
-  let productosJSON = localStorage.getItem("bodega");
-  let productos = JSON.parse(productosJSON);
+    let productosJSON = localStorage.getItem("bodega");
+    let productos = JSON.parse(productosJSON);
+    console.log(productos);
 
-  let miNuevoProducto = {
+    let miNuevoProducto = {
       id: bodega.length + 1,
       nombre,
-      precio,
-      cantidad,
+      precio: parseFloat(precio),
+      cantidad: parseInt(cantidad),
+    };
+
+    productos.push(miNuevoProducto);
+
+    localStorage.setItem("bodega", JSON.stringify());
   };
 
-  productos.push(miNuevoProducto);
+  let lista = document.getElementById("guardar");
+  lista.addEventListener("submit", (e) => {
+    e.preventDefault();
+    agregoABodega(lista.nombre.value, lista.precio.value, lista.cantidad.value);
 
-   localStorage.setItem("bodega", JSON.stringify(bodega));
- };
-
-let lista = document.getElementById("guardar");
-lista.addEventListener("submit", (e)) => {
-e.preventDefault();
-}
+  });
